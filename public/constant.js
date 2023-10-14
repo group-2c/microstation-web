975da3aa9714ca8468fc47775f4c7bd50abe6584
Chan <1320729601@qq.com>
2023-10-14 15:09:40 +0800

    服务器地址变更

--- public/constant.js
+++ public/constant.js
@@ -1,6 +1,6 @@
 export default {
-  VUE_APP_NAME: "智能微站综合管理云平台",
-  BASE_URL: "http://47.93.197.138:30046/api",
-  VUE_APP_GLOBAL_VAR: "MICROSTATION",
-  VUE_APP_COPYRIGHT: "云南省交通规划设计研究院有限公司"
+    VUE_APP_NAME: '智能微站综合管理云平台',
+    BASE_URL: 'http://49.232.189.150:30046/api',
+    VUE_APP_GLOBAL_VAR: 'MICROSTATION',
+    VUE_APP_COPYRIGHT: '云南省交通规划设计研究院有限公司',
 } 
