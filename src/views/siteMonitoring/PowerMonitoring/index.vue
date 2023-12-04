a080a9635964f43d3d1fb5cbb592b138e1a48a48
Cellular <724661571@qq.com>
2023-12-04 16:45:52 +0800

    电力监控展示页面

--- src/views/siteMonitoring/PowerMonitoring/index.vue
+++ src/views/siteMonitoring/PowerMonitoring/index.vue
@@ -5,9 +5,111 @@
  * @filePath: src/views/siteMonitoring/PowerMonitoring/index.vue
  * @description: 电力监控
  */
+<style lang="less" scoped>
+@import url("./index.less");
+</style>
+
 <template>
-  <div>111</div>
+  <div class="parcel">
+    <a-page-header class="pageHeader" title="电力监控" />
+    <div class="contentContainer">
+      <div class="microstationContainer">
+        <div class="leftArea">
+          <a-list :data-source="modelList" :bordered="true">
+            <template #header>
+              <div>模型列表</div>
+            </template>
+            <template #renderItem="{ item }">
+              <a-list-item :class="currentModel?.id === item.id ? 'active' : ''" @click="modelItemClick(item)" >
+                <div class="name">{{ item.name }}</div>
+              </a-list-item>
+            </template>
+          </a-list>
+        </div>
+        <div class="powerDistribution" style="height: calc(100vh - 244px);">
+          <a-spin :spinning="loading">
+            <div class="modelContainer" ref="modelRef"></div>
+          </a-spin>
+          <div v-if="!currentModel?.id" class="empty">暂无模型数据</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue"
+import { message } from "ant-design-vue"
+import fileApi from "_api/files"
+import PowerModel from "./model"
+
+const loading = ref(false)
+const modelList = ref([])
+const currentModel = ref({})
+
+const modelRef = ref()
+
+const _getModelList = async () => {
+  const loading = message.loading("正在加载模型数据...", 0)
+  try {
+    modelList.value = [
+      {
+        id: 1,
+        name: "模型101",
+        fileName: "NoLod_0_1701675987233.glb"
+      },
+      {
+        id: 2,
+        name: "模型102",
+        fileName: "NoLod_1_1701676020419.glb"
+      },
+      {
+        id: 3,
+        name: "模型103",
+        fileName: "NoLod_0_1701675987233.glb"
+      },
+      {
+        id: 4,
+        name: "模型104",
+        fileName: "NoLod_1_1701676020419.glb"
+      }
+    ]
+  } catch (err) {
+    message.error(`模型数据加载失败:${err}`)
+  } finally {
+    setTimeout(loading)
+  }
+}
+
+const _initModelView = url => {
+  const powerModel = new PowerModel()
+  powerModel.initModelView(modelRef.value, url, () => {
+    console.log("加载完成！")
+  })
+}
+
+const _getFileUrl = async fileName => {
+  const _loading = message.loading("正在加载模型...", 0)
+  loading.value = true
+  try {
+    const res = await fileApi.filePreview({ fileName })
+    console.log(res.data.url)
+    _initModelView(res.data.url)
+  } catch (err) {
+    console.error(err)
+    message.error(`加载模型失败:${err}`)
+  } finally {
+    loading.value = false
+    setTimeout(_loading)
+  }
+}
+
+const modelItemClick = item => {
+  currentModel.value = item
+  _getFileUrl(item.fileName)
+}
+
+onMounted(() => {
+  _getModelList()
+})
 </script>
---
+++ src/views/siteMonitoring/PowerMonitoring/model.js
@@ -0,0 +1,73 @@
+/*
+ * @author: zzp
+ * @date: 2023-12-04 15:59:27
+ * @fileName: model.js
+ * @filePath: src/views/siteMonitoring/PowerMonitoring/model.js
+ * @description: 模型处理相关
+ */
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { RoomEnvironment } from "three/examples/jsm/environments/RoomEnvironment.js"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
+class PowerModel {
+  mixer
+  clock
+  renderer
+  scene
+  camera
+  controls
+  model
+
+  _animate = () => {
+    requestAnimationFrame(this._animate)
+    const delta = this.clock.getDelta()
+    this.mixer.update(delta)
+
+    this.controls.update()
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  initModelView = (container, url, callBack) => {
+    this.clock = new THREE.Clock()
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    container.appendChild(this.renderer.domElement)
+
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer)
+
+    this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(0xbfe3dd)
+    this.scene.environment = pmremGenerator.fromScene(new RoomEnvironment(this.renderer), 0.04).texture
+
+    this.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 100)
+    this.camera.position.set(5, 2, 8)
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.controls.target.set(0, 0.5, 0)
+    this.controls.update()
+    this.controls.enablePan = false
+    this.controls.enableDamping = true
+
+    const manager = new THREE.LoadingManager()
+    manager.onLoad = () => {
+      callBack && callBack()
+    }
+
+    const loader = new GLTFLoader(manager)
+    loader.load(url, gltf => {
+      this.model = gltf.scene
+      this.model.position.set(1, 1, 0)
+      this.scene.add(this.model)
+
+      this.mixer = new THREE.AnimationMixer(this.model)
+      this.mixer.clipAction(gltf.animations[0]).play()
+
+      this._animate()
+    })
+  }
+}
+
+export default PowerModel
